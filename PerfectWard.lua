31df294684f8feddc7ebc404cd13febc7c0978f8
MLStudio <mlstudio2014@gmail.com>
2014-11-21 13:21:50 -0500

    Fixed autoupdater

--- PerfectWard.lua
+++ PerfectWard.lua
@@ -10,36 +10,33 @@ local version = "0.1"
 
 
 
-local autoupdateenabled = true
+local AUTOUPDATE = true
 local UPDATE_SCRIPT_NAME = "PerfectWard"
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/MLStudio/BoL/master/PerfectWard.lua"
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-local ServerData
-if autoupdateenabled then
-    GetAsyncWebResult(UPDATE_HOST, UPDATE_PATH, function(d) ServerData = d end)
-    function update()
-        if ServerData ~= nil then
-            local ServerVersion
-            local send, tmp, sstart = nil, string.find(ServerData, "local version = \"")
-            if sstart then
-                send, tmp = string.find(ServerData, "\"", sstart+1)
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Perfect Ward:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+    local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
+    if ServerData then
+        PrintChat(tostring(ServerData))
+        local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
+        ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
+        if ServerVersion then
+            ServerVersion = tonumber(ServerVersion)
+            if tonumber(version) < ServerVersion then
+                AutoupdaterMsg("New version available"..ServerVersion)
+                AutoupdaterMsg("Updating, please don't press F9")
+                DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+            else
+                AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
             end
-            if send then
-                ServerVersion = tonumber(string.sub(ServerData, sstart+1, send-1))
-            end
-
-            if ServerVersion ~= nil and tonumber(ServerVersion) ~= nil and tonumber(ServerVersion) > tonumber(version) then
-                DownloadFile(UPDATE_URL.."?nocache"..myHero.charName..os.clock(), UPDATE_FILE_PATH, function () print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> successfully updated. Reload (double F9) Please. ("..version.." => "..ServerVersion..")</font>") end)     
-            elseif ServerVersion then
-                print("<font color=\"#FF0000\"><b>"..UPDATE_SCRIPT_NAME..":</b> You have got the latest version: <u><b>"..ServerVersion.."</b></u></font>")
-            end     
-            ServerData = nil
         end
+    else
+        AutoupdaterMsg("Error downloading version info")
     end
-    AddTickCallback(update)
 end
 
 --[[
