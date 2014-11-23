c2110de11c2e44def5959eb1a81f0d39e4693f56
MLStudio <mlstudio2014@gmail.com>
2014-11-23 09:30:43 -0500

    Added jungle steal with E

--- MLS-Kalista.lua
+++ MLS-Kalista.lua
@@ -1,4 +1,4 @@
-local version = "0.21"
+local version = "0.3"
 
 
 if myHero.charName ~= "Kalista" then return end
@@ -8,6 +8,7 @@ MLStudio's Kalista v0.1 beta
 
 v0.1 -- First release
 v0.2 -- More accurate E dmg calculation and VIP_USER check
+v0.3 -- Added jungle steal with E
 
 --]]
 
@@ -56,6 +57,80 @@ local ts = nil
 local tsE = nil
 local SpellRangedQ = {Range = 1450, Speed = 1800, Delay = 0.2, Width = 50}
 
+local monsters = {
+	{	-- baron
+		name = "baron",
+		spawn = 900,
+		respawn = 420,
+		advise = true,
+		camps = {
+			{
+				pos = { x = 4960, y = 60, z = 10420 },
+				name = "monsterCamp_12",
+				creeps = { { { name = "SRU_Baron12.1.1" }, }, },
+				team = TEAM_NEUTRAL,
+			},
+		},
+	},
+	{	-- dragon
+		name = "dragon",
+		spawn = 150,
+		respawn = 360,
+		advise = true,
+		camps = {
+			{
+				pos = { x = 9846, y = 60, z = 4449 },
+				name = "monsterCamp_6",
+				creeps = { { { name = "SRU_Dragon6.1.1" }, }, },
+				team = TEAM_NEUTRAL,
+			},
+		},
+	},
+	{	-- red
+		name = "red",
+		spawn = 115,
+		respawn = 300,
+		advise = true,
+		camps = {
+			{
+				pos = { x = 7832, y = 60, z = 4173 },
+				name = "monsterCamp_4",
+				creeps = { { { name = "SRU_Red4.1.1" }, }, },
+				team = TEAM_BLUE,
+			},
+			{
+				pos = { x = 7031, y = 60, z = 10875 },
+				name = "monsterCamp_10",
+				creeps = { { { name = "SRU_Red10.1.1" }, }, },
+				team = TEAM_RED,
+			},
+		},
+	},
+	{	-- blue
+		name = "blue",
+		spawn = 115,
+		respawn = 300,
+		advise = true,
+		camps = {
+			{
+				pos = { x = 3873, y = 60, z = 8007 },
+				name = "monsterCamp_1",
+				creeps = { { { name = "SRU_Blue1.1.1" }, }, },
+				team = TEAM_BLUE,
+			},
+			{
+				pos = { x = 10935, y = 60, z = 7017 },
+				name = "monsterCamp_7",
+				creeps = { { { name = "SRU_Blue7.1.1" }, }, },
+				team = TEAM_RED,
+			},
+		},
+	},
+}
+
+function firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
 
 function Menu()
 	ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, SpellRangedQ.Range, DAMAGE_PHYSICAL)
@@ -70,6 +145,7 @@ function Menu()
     Config:addParam("ks", "KS with E", SCRIPT_PARAM_ONOFF, true)
     Config:addSubMenu("Combo options", "ComboSub")
     Config:addSubMenu("Harass options", "HarassSub")
+    Config:addSubMenu("KS options", "KSSub")
     Config:addSubMenu("Draw", "Draw")
     Config:addSubMenu("Extra", "Extra")
     Config.ComboSub:addParam("useQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
@@ -80,6 +156,16 @@ function Menu()
     Config.HarassSub:addParam("autoERange", "Auto E on max range", SCRIPT_PARAM_ONOFF, true)
     Config.HarassSub:addParam("autoE", "Auto E on stacks reached", SCRIPT_PARAM_ONOFF, true)
     Config.HarassSub:addParam("autoEStacks", "Number of stacks reached: ", SCRIPT_PARAM_SLICE, 10, 1, 15, 0)
+    Config.KSSub:addParam("ks", "Champion KS with E", SCRIPT_PARAM_ONOFF, true)
+    Config.KSSub:addParam("jungle", "Secure jungle with E", SCRIPT_PARAM_ONOFF, true)
+    Config.KSSub:addSubMenu("Jungle Creeps to Steal", "creepList")
+    for i,monster in pairs(monsters) do
+    	Config.KSSub.creepList:addParam(monster.name,firstToUpper(monster.name),SCRIPT_PARAM_ONOFF, true)
+    end
+    -- Config.KSSub.creepList:addParam("baron","Baron", SCRIPT_PARAM_ONOFF, true)
+    -- Config.KSSub.creepList:addParam("dragon","Dragon", SCRIPT_PARAM_ONOFF, true)
+    -- Config.KSSub.creepList:addParam("red","Red", SCRIPT_PARAM_ONOFF, true)
+    -- Config.KSSub.creepList:addParam("blue","Blue", SCRIPT_PARAM_ONOFF, true)
     Config.Draw:addParam("drawE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
     Config.Extra:addParam("debug", "Debug Mode", SCRIPT_PARAM_ONOFF, false)
     Config.Extra:addParam("packetCast", "Packet Cast Spells", SCRIPT_PARAM_ONOFF, true)
@@ -97,12 +183,87 @@ function OnLoad()
         end
     end
 
+    for i = 1, objManager.maxObjects do --if jungle creep is in objectmanage, lets "process" it with addcampcreep function
+		local object = objManager:getObject(i)
+		if object ~= nil then
+			addCampCreepAltar(object)
+		end
+	end
+
+	-- for i,monster in pairs(monsters) do
+	-- 	monster.advise = true
+	-- end
+
+	AddCreateObjCallback(addCampCreepAltar)  --make sure items that are created or destroyed are processed properly
+	AddDeleteObjCallback(removeCreep)
+
 end
 
+function addCampCreepAltar(object)
+	if object ~= nil and object.name ~= nil then
+		for i,monster in pairs(monsters) do
+			for j,camp in pairs(monster.camps) do
+				if camp.name == object.name then
+					--PrintChat(object.name) --setting monsterCamp
+					camp.object = object
+					return
+				end
+				if object.type == "obj_AI_Minion" then  --do this most of the time for SR I guess
+					for k,creepPack in ipairs(camp.creeps) do
+						for l,creep in ipairs(creepPack) do
+							if object.name == creep.name or object.name:find(creep.name) then
+								creep.object = object  --set the creep list object to actual object in game
+								creep.stack = 0
+								creep.time = 0
+								return
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function removeCreep(object)
+	if object ~= nil and object.type == "obj_AI_Minion" and object.name ~= nil then
+		for i,monster in pairs(monsters) do --name == baron level
+			for j,camp in pairs(monster.camps) do -- monster_camp# level
+				for k,creepPack in ipairs(camp.creeps) do --group of creeps
+					for l,creep in ipairs(creepPack) do --individual creep
+						if object.name == creep.name or object.name:find(creep.name) then
+							creep.object = nil --set jungle list object to nil
+							return
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+local lastTick = 0
 function OnTick()
 	ts:update()
 	tsE:update()
 	local Elvl = myHero:GetSpellData(_E).level
+
+	if GetTickCount() > lastTick or 1 then
+		debugMSG = debugMSG .. '\n'
+		for i= 1, #monsters do
+			local monster = monsters[i]
+			monster.advise = Config.KSSub.creepList[monster.name]
+			-- if monster.advise then
+			-- 	PrintChat(monster.name .. " is true!")
+			-- end
+			-- if Config.Extra.debug then
+			-- 	debugMSG = debugMSG .. monster.name .. ': ' .. tostring(monster.advise)
+			-- end
+		end
+		-- debugMSG = debugMSG .. '\n'
+		lastTick = GetTickCount() + 2000
+	end
+
 
 	debugMSG = "debug: "
 	for i, target in pairs(enemyHeroes) do --clear timedout stacks
@@ -115,7 +276,7 @@ function OnTick()
 		end
 	end
 
-	if Config.ks then
+	if Config.KSSub.ks then
 		for i, target in pairs(enemyHeroes) do
 			if ValidTarget(target.object, 1000) and target.stack > 0 then
 				-- local dmg = getDmg("E", target.object, myHero, 3) 
@@ -137,6 +298,27 @@ function OnTick()
 				end
 			end
 		end 
+	end
+
+	if Config.KSSub.jungle then
+		for i,monster in pairs(monsters) do --name = baron level
+			for j,camp in pairs(monster.camps) do
+				for k,creepPack in ipairs(camp.creeps) do
+					for l,creep in ipairs(creepPack) do
+						if creep.object ~= nil and creep.object.valid and creep.object.dead == false and creep.stack > 0 and monster.advise then
+							local dmg = eDmgCalc(creep.object,creep.stack)
+							if dmg >= creep.object.health then
+								if Config.Extra.packetCast and VIP_USER then
+									packetCast(_E)
+								else
+									CastSpell(_E)
+								end
+							end
+						end
+					end
+				end
+			end
+		end
 	end
 
 	if Config.Combo then
@@ -364,6 +546,49 @@ function OnCreateObj(obj)
 		 	end
 		end
 	end
+
+	for i,monster in pairs(monsters) do --name = baron level
+		for j,camp in pairs(monster.camps) do
+			for k,creepPack in ipairs(camp.creeps) do
+				for l,creep in ipairs(creepPack) do
+					if creep.object ~= nil and creep.object.valid and creep.object.dead == false and GetDistance(creep.object,obj) < 80 then
+						if obj.name == "Kalista_Base_E_Spear_tar6.troy" then
+			 				if creep.stack < 6 then
+			 					creep.stack = 6
+			 				end
+			 				creep.time = GetGameTimer()
+
+					 	elseif obj.name == "Kalista_Base_E_Spear_tar5.troy" then
+					 		if creep.stack < 6 then
+			 					creep.stack = 5
+			 				end
+					 		creep.time = GetGameTimer()
+					 	elseif obj.name == "Kalista_Base_E_Spear_tar4.troy" then
+					 		if creep.stack < 6 then
+			 					creep.stack = 4
+			 				end
+					 		creep.time = GetGameTimer()
+					 	elseif obj.name == "Kalista_Base_E_Spear_tar3.troy" then
+					 		if creep.stack < 6 then
+			 					creep.stack = 3
+			 				end
+					 		creep.time = GetGameTimer()
+					 	elseif obj.name == "Kalista_Base_E_Spear_tar2.troy" then
+					 		if creep.stack < 6 then
+			 					creep.stack = 2
+			 				end
+					 		creep.time = GetGameTimer()
+					 	elseif obj.name == "Kalista_Base_E_Spear_tar1.troy" then
+					 		if creep.stack < 6 then
+			 					creep.stack = 1
+			 				end
+					 		creep.time = GetGameTimer()
+					 	end
+					end
+				end
+			end
+		end
+	end
 end
 
 function OnProcessSpell(unit, spell) 
@@ -375,6 +600,16 @@ function OnProcessSpell(unit, spell)
         	end
         end
 
+        for i,monster in pairs(monsters) do --name = baron level
+			for j,camp in pairs(monster.camps) do
+				for k,creepPack in ipairs(camp.creeps) do
+					for l,creep in ipairs(creepPack) do
+						creep.stack = 0
+					end
+				end
+			end
+		end
+
         if spell.name:lower():find("attack") then
         	--PrintChat(spell.target.name)
         	--PrintChat(tostring(spell.windUpTime))
@@ -385,6 +620,20 @@ function OnProcessSpell(unit, spell)
         			end
         		end
         	end
+
+        	for i,monster in pairs(monsters) do --name = baron level
+				for j,camp in pairs(monster.camps) do
+					for k,creepPack in ipairs(camp.creeps) do
+						for l,creep in ipairs(creepPack) do
+							if spell.target == creep.object then
+								if creep.stack > 5 then
+									DelayAction(function() creep.stack = creep.stack + 1 end,spell.windUpTime)
+								end
+							end
+						end
+					end
+				end
+			end
         end
     end
 end
